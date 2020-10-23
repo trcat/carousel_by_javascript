325bb3566d91b5f14d8224cffe342af89dc0b2f5
DayDay <daydaybigking@sina.com>
2020-10-23 14:06:50 +0800

    实现循环

--- index.js
+++ index.js
@@ -1,19 +1,18 @@
 const container = document.querySelector(".carousel-container");
 const wrap = document.querySelector(".carousel-wrap");
+const cloneFirst = document.querySelector('.clone-first');
 wrap.style.position = "relative";
 let distance = 0;
-const viewBoxWidth = container.clientWidth;
-const wrapWidth = wrap.offsetWidth;
-const translateLimit = wrapWidth - viewBoxWidth;
+// 第一个克隆元素到 wrap 最左边的距离就是单次轮播的距离
+const translateLimit = cloneFirst.offsetLeft;
+const speed = translateLimit / 10000;
 
 // 设定 timer 并开始轮播
 let timer;
 function callback() {
   window.clearTimeout(timer);
-  if (Math.abs(distance) !== translateLimit) {
-    distance -= 1;
-    wrap.style.transform = `translateX(${distance}px)`;
-    timer = window.setTimeout(callback, 1);
-  }
+  Math.abs(distance) >= translateLimit ? (distance = 0) : (distance -= speed);
+  wrap.style.transform = `translateX(${distance}px)`;
+  window.setTimeout(callback, 1);
 }
 timer = window.setTimeout(callback);
