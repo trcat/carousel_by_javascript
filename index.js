bd678298cac5cfe7a3473088b4151744ef0a3572
DayDay <daydaybigking@sina.com>
2020-10-23 11:43:55 +0800

    轮播显示最后一个内容就停止轮播

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
+const container = document.querySelector(".carousel-container");
 const wrap = document.querySelector(".carousel-wrap");
 wrap.style.position = "relative";
 let distance = 0;
+const viewBoxWidth = container.clientWidth;
+const wrapWidth = wrap.offsetWidth;
+const translateLimit = wrapWidth - viewBoxWidth;
+console.log(translateLimit);
 
+// 设定 timer 并开始轮播
 const timer = window.setInterval(() => {
-  distance -= 1;
-  wrap.style.transform = `translateX(${distance}px)`;
+  if (Math.abs(distance) === translateLimit) {
+    window.clearInterval(timer);
+  } else {
+    distance -= 1;
+    wrap.style.transform = `translateX(${distance}px)`;
+  }
 }, 1);
