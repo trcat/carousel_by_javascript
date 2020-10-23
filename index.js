70adf6cfcc5f24739a9dada30cdbc30543835fed
DayDay <daydaybigking@sina.com>
2020-10-23 14:28:45 +0800

    修复循环bug

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const container = document.querySelector(".carousel-container");
 const wrap = document.querySelector(".carousel-wrap");
-const cloneFirst = document.querySelector('.clone-first');
+const cloneFirst = document.querySelector(".clone-first");
 let distance = 0;
 // 第一个克隆元素到 wrap 最左边的距离就是单次轮播的距离
 const translateLimit = cloneFirst.offsetLeft;
 const speed = translateLimit / 10000;
+console.log(translateLimit);
+console.log(cloneFirst.offsetParent);
 
 // 设定 timer 并开始轮播
 let timer;
@@ -12,6 +14,9 @@ function callback() {
   window.clearTimeout(timer);
   Math.abs(distance) >= translateLimit ? (distance = 0) : (distance -= speed);
   wrap.style.transform = `translateX(${distance}px)`;
-  window.setTimeout(callback, 1);
+  timer = window.setTimeout(callback, 1);
 }
 timer = window.setTimeout(callback);
+
+// wrap 添加 mouseover 和 mouseleave 事件
+wrap.onmouseover = () => {};
