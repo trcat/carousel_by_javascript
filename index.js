75a0f31b88888e17d216b0a3f7196892fb193d49
DayDay <daydaybigking@sina.com>
2020-10-23 14:57:44 +0800

    添加自右向左轮播

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 const container = document.querySelector(".carousel-container");
 const wrap = document.querySelector(".carousel-wrap");
 const cloneFirst = document.querySelector(".clone-first");
-let distance = 0;
 // 第一个克隆元素到 wrap 最左边的距离就是单次轮播的距离
 const translateLimit = cloneFirst.offsetLeft;
 const speed = translateLimit / 10000;
 // 标记当前轮播方向, 默认为 left, 自左向右
-let direction = "left";
+let direction = "right";
+let distance;
+if (direction === "left") {
+  distance = 0;
+} else {
+  distance = translateLimit * -1;
+  console.log(distance)
+  wrap.style.transform = `translateX(${distance}px)`;
+}
 
 // 设定 timer 并开始轮播
 let timer;
@@ -15,14 +22,23 @@ function leftCallback() {
   window.clearTimeout(timer);
   Math.abs(distance) >= translateLimit ? (distance = 0) : (distance -= speed);
   wrap.style.transform = `translateX(${distance}px)`;
-  timer = window.setTimeout(callback, 1);
+  timer = window.setTimeout(leftCallback, 1);
 }
-timer = window.setTimeout(leftCallback);
+function rightCallback() {
+  // 释放上一次 timeout
+  window.clearTimeout(timer);
+  distance >= 0 ? (distance = translateLimit * -1) : (distance += speed);
+  wrap.style.transform = `translateX(${distance}px)`;
+  timer = window.setTimeout(rightCallback, 1);
+}
+timer = window.setTimeout(direction === "left" ? leftCallback : rightCallback);
 
 // wrap 添加 mouseover 和 mouseleave 事件
 wrap.onmouseover = () => {
   window.clearInterval(timer);
 };
 wrap.onmouseleave = () => {
-  timer = window.setTimeout(leftCallback);
+  timer = window.setTimeout(
+    direction === "left" ? leftCallback : rightCallback
+  );
 };
