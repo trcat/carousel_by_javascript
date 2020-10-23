ace87ca745243d090b8f6b44d953c421b63678a3
DayDay <daydaybigking@sina.com>
2020-10-23 13:15:12 +0800

    用 settime out 替代 setInterval 实现

--- index.js
+++ index.js
@@ -5,14 +5,15 @@ let distance = 0;
 const viewBoxWidth = container.clientWidth;
 const wrapWidth = wrap.offsetWidth;
 const translateLimit = wrapWidth - viewBoxWidth;
-console.log(translateLimit);
 
 // 设定 timer 并开始轮播
-const timer = window.setInterval(() => {
-  if (Math.abs(distance) === translateLimit) {
-    window.clearInterval(timer);
-  } else {
+let timer;
+function callback() {
+  window.clearTimeout(timer);
+  if (Math.abs(distance) !== translateLimit) {
     distance -= 1;
     wrap.style.transform = `translateX(${distance}px)`;
+    timer = window.setTimeout(callback, 1);
   }
-}, 1);
+}
+timer = window.setTimeout(callback);
